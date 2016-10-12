ad36764af43123b939038a38cfeecf63724087f3
R. Flohil <renef95@hotmail.com>
2016-10-12 16:25:00 +0200

    Trainen af, begonnen aan test

--- assignment3/clus_nowarnings/KMeans.java
+++ assignment3/clus_nowarnings/KMeans.java
@@ -53,6 +53,7 @@ public class KMeans extends ClusteringAlgorithm
 		for (int ic = 0; ic < k; ic++)
 			clusters[ic] = new Cluster();
 	}
+  
   /// Calculates the mean of a cluster
   public float[] calculatePrototype(int clusterNumber)
   {
@@ -67,10 +68,7 @@ public class KMeans extends ClusteringAlgorithm
       }
     }
     /// Divide the prototype's values by the amount of members to get the mean
-    for (int value = 0; value < 200; ++value)
-    {
-      prototype[value] /= trainData.size();
-    }
+    for (int value = 0; value < 200; ++value) prototype[value] /= trainData.size();
     return prototype;
   }
 
@@ -78,66 +76,98 @@ public class KMeans extends ClusteringAlgorithm
 	{
 	 	//implement k-means algorithm here:
 		// Step 1: Select an initial random partioning with k clusters
-    Random rand = new Random(); /// Create a random number generator
-    for(int i = 0; i < trainData.size(); ++i) /// Loop from 0 to the last user
-    {
-      clusters[rand.nextInt(k)].currentMembers.add(i); /// Add the user to a random cluster
-    }
-		// Step 2: Generate a new partition by assigning each datapoint to its closest cluster center
-    /// For all clusters calculate its mean
-    for(int i = 0; i < k; ++i)
-    {
-      clusters[k].prototype = calculatePrototype(k);
-      /// Set all members to the previous members  and clear the current
-      // TODO: to be moved?
-      clusters[k].previousMembers = clusters[k].currentMembers;
-      clusters[k].currentMembers.clear();
-    }
-
+    // Step 2: Generate a new partition by assigning each datapoint to its closest cluster center
+    // Step 3: recalculate cluster centers
+    // Step 4: repeat until clustermembership stabilizes
     
-    /// For every member in the trainingset choose it's closest prototype
-    for(int member = 0; member < trainData.size(); ++member)
+    boolean stable = false;
+    
+    // Step 1
+    Random rand = new Random();                         /// Create a random number generator
+    for (int i = 0; i < trainData.size(); ++i)          /// Loop from 0 to the last user
     {
-      /// Variables for saving the current best cluster and current smallest distance
-      int bestCluster = -1;
-      double smallestDist = Double.POSITIVE_INFINITY;
-      /// For every cluster
-      for(int currentCluster = 0; currentCluster < k; ++currentCluster)
+      clusters[rand.nextInt(k)].currentMembers.add(i);  /// Add the user to a random cluster
+    }
+    
+    /// For all clusters calculate its mean
+    for (int i = 0; i < k; ++i) clusters[k].prototype = calculatePrototype(k);
+    
+    // Step 4
+    while (!stable)
+    {
+      // Step 2
+      /// For every member in the trainingset choose it's closest prototype
+      for(int member = 0; member < trainData.size(); ++member)
       {
-        /// Set the distance at 0, it will be incremented
-        double distance = 0;
-        /// Calculate the Euclidean distance between the member and the current cluster its prototype
-        for (int value = 0; value < 200; ++value)
-          distance += Math.pow(trainData.get(member)[value] - clusters[currentCluster].prototype[value], 2);
-        distance = Math.sqrt(distance);
-        /// Is the calculated distance the smallest? if so alter the best cluster choice
-        if (distance < smallestDist) bestCluster = currentCluster;
+        /// Variables for saving the current best cluster and current smallest distance
+        int bestCluster = -1;
+        double smallestDist = Double.POSITIVE_INFINITY;
+        for(int currentCluster = 0; currentCluster < k; ++currentCluster) /// For every cluster
+        {
+          double distance = 0; /// Set the distance at 0, it will be incremented
+          /// Calculate the Euclidean distance between the member and the current cluster its prototype
+          for (int value = 0; value < 200; ++value)
+            distance += Math.pow(trainData.get(member)[value] - clusters[currentCluster].prototype[value], 2);
+          distance = Math.sqrt(distance);
+          /// Is the calculated distance the smallest? if so: alter the best cluster choice
+          if (distance < smallestDist) 
+          {
+            bestCluster = currentCluster; /// Alter the best cluster choice
+            smallestDist = distance;      /// Set the new smallest distance
+          }
+        }
+        clusters[bestCluster].currentMembers.add(member);
       }
-      clusters[bestCluster].currentMembers.add(member);
+      // Step 3
+      for(int i = 0; i < k; ++i)                        /// For every cluster
+      {
+        clusters[i].prototype = calculatePrototype(i);  /// Calculate the prototype
+        // Step 4
+        /// Check whether the set has changed since the last iteration
+        if (!clusters[i].currentMembers.equals(clusters[i].previousMembers)) stable = false;
+      }
+      /// If membership is not stable set all members to the previous members and clear the current set
+      if (!stable)
+      {
+        for(int i = 0; i < k; ++i)
+        {
+          clusters[i].previousMembers = clusters[i].currentMembers;
+          clusters[i].currentMembers.clear();
+        }
+      }
     }
-		// Step 3: recalculate cluster centers
-    for(int i = 0; i < k; ++i)
-    {
-      clusters[k].prototype = calculatePrototype(k);
-      /// Set all members to the previous members  and clear the current
-      // TODO: to be moved?
-      clusters[k].previousMembers = clusters[k].currentMembers;
-      clusters[k].currentMembers.clear();
-    }
-		// Step 4: repeat until clustermembership stabilizes
 		return false;
 	}
 
 	public boolean test()
 	{
-		// iterate along all clients. Assumption: the same clients are in the same order as in the testData
-		// for each client find the cluster of which it is a member
-		// get the actual testData (the vector) of this client
-		// iterate along all dimensions
-		// and count prefetched htmls
-		// count number of hits
-		// count number of requests
-		// set the global variables hitrate and accuracy to their appropriate value
+    int memberCluster = -1, hitrate, accuracy;
+    
+    // iterate along all clients. Assumption: the same clients are in the same order as in the testData
+    for (int member = 0; member < trainData.size(); ++member)
+    {
+      int hits = 0;
+      // for each client find the cluster of which it is a member
+      for (int currentCluster = 0; currentCluster < k; ++currentCluster) 
+        if (clusters[currentCluster].currentMembers.contains(member)) memberCluster = currentCluster;
+            
+      // get the actual testData (the vector) of this client
+      // iterate along all dimensions
+      // and count prefetched htmls
+      // count number of hits
+      // count number of requests
+      for (int value = 0; value < 200; ++value)
+      {
+        if (testData.get(member)[value] == clusters[memberCluster].prototype[value])
+        {
+          ++hits;
+        }
+      }
+
+      // set the global variables hitrate and accuracy to their appropriate value
+    }
+		
+
 		return true;
 	}
 
